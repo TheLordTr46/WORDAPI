caea1998f67f00f0235de73d79ee571883a7d4f9
Ahmet Anil Sariguzel <ahmet5546anil@gmail.com>
2026-02-06 00:48:11 +0300

    Düzeltme yapıldı

--- words/apps.py
+++ words/apps.py
@@ -1,28 +1,24 @@
 from django.apps import AppConfig
 from django.contrib.auth import get_user_model
-import os
 
 class WordsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'words'
 
     def ready(self):
-        # Sadece production'da çalışsın (Render)
-        if os.environ.get("RENDER") != "true":
-            return
-
         User = get_user_model()
 
-        username = os.environ.get("ADMIN_USERNAME")
-        email = os.environ.get("ADMIN_EMAIL")
-        password = os.environ.get("ADMIN_PASSWORD")
+        USERNAME = "admin"
+        EMAIL = "admin@wordapi.com"
+        PASSWORD = "Admin123!"
 
-        if not username or not password:
-            return
-
-        if not User.objects.filter(username=username).exists():
-            User.objects.create_superuser(
-                username=username,
-                email=email,
-                password=password
-            )
+        try:
+            if not User.objects.filter(username=USERNAME).exists():
+                User.objects.create_superuser(
+                    username=USERNAME,
+                    email=EMAIL,
+                    password=PASSWORD
+                )
+        except Exception:
+            # migrate sırasında model henüz hazır olmayabilir
+            pass
